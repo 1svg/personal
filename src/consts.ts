c5f3f1ac353de785e9e97886101f0731eb9bdd64
Florian <f.boittiaux@icloud.com>
2026-06-02 16:36:51 +0200

    fix: reorder index layout

--- src/consts.ts
+++ src/consts.ts
@@ -16,14 +16,6 @@ export const NAV_LINKS: SocialLink[] = [
     href: '/tools',
     label: 'tools',
   },
-  {
-    href: '/blog',
-    label: 'blog',
-  },
-  {
-    href: '/about',
-    label: 'about',
-  },
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
